722093adee44bebbe22117d0d42c9150ba2d9ada
BadAss <never4575@naver.com>
2025-11-09 00:45:11 +0900

    feat(config): add ModelConfig class for model configuration settings

---
+++ model/config/config.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass, field
+import torch
+
+@dataclass
+class ModelConfig:
+    input_size: int = 14
+    input_feature_variants: int = 1
+    window_size: int = 100
+    dim_model: int = 512
+    dim_ff: int = 1024
+    num_layers: int = 3 # 6
+    num_heads: int = 4 # 8
+    dropout: float = 0.15
+    batch_size: int = 64
+    epoch: int = 200
+    future_offset: list = field(default_factory=lambda: [1, 3, 7, 14, 30])
+    val_clip_range: int = 150
+    feature_cols: list = field(default_factory=lambda: [
+        'open', 'close',
+        'volume',
+        'macd', 'signal_line', 'macd_hist',
+        'sma_10', 'sma_20', 'sma_50',
+        'upper_band', 'lower_band', 'bollinger_width',
+        'rsi'
+        ])
+    target_cols: list = field(default_factory=lambda: [
+        'close'
+    ])
+    error_threshold: float = 0.02
+    learning_rate: float = 0.002
+    min_learning_rate: float = 0.0001
+    pad_token_id: float = 0.0
+    device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+    seed: int = 218
+
+    def __post_init__(self):
+        self.all_cols = self.feature_cols.copy()
+        for col in self.target_cols:
+            if col not in self.all_cols:
+                self.all_cols.append(col)
+
+        total_weight = sum(self.signal_loss_weights)
+        self.signal_loss_weights = [w / total_weight / 10 for w in self.signal_loss_weights]
+
+        self.input_size = len(self.feature_cols) + 1
+        self.dim_head = self.dim_model // self.num_heads
